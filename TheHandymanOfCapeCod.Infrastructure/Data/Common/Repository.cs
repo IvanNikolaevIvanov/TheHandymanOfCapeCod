ce80642c1388ad63d967a77f5aa29930b017940a
bluesbreak <ivanivanovat@gmail.com>
2024-05-08 18:41:00 +0300

    Unit Of Work Repository pattern added.

---
+++ TheHandymanOfCapeCod.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using The_Handyman_Of_Cape_Cod.Infrastructure.Data;
+
+namespace TheHandymanOfCapeCod.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(TheHandymanOfCapeCodDb _context)
+        {
+             context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+
+            if (entity != null)
+            {
+                DbSet<T>().Remove(entity);
+            }
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
