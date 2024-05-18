d870b0d3f343151e3ebd763797265378770f1ae9
bluesbreak <ivanivanovat@gmail.com>
2024-05-18 18:35:36 +0300

    Upload Photo functionality added

--- The Handyman Of Cape Cod/Areas/Admin/Controllers/PhotoController.cs
+++ The Handyman Of Cape Cod/Areas/Admin/Controllers/PhotoController.cs
@@ -7,21 +7,31 @@ namespace The_Handyman_Of_Cape_Cod.Areas.Admin.Controllers
     public class PhotoController : AdminBaseController
     {
         private readonly IPhotoService photoService;
+        private readonly IProjectService projectService;
 
-        public PhotoController(IPhotoService _photoService)
+        public PhotoController(
+            IPhotoService _photoService,
+            IProjectService _projectService)
         {
-             photoService = _photoService;
+            photoService = _photoService;
+            projectService = _projectService;
         }
 
         [HttpPost]
         public async Task<IActionResult> AddPhotos(int id)
         {
+            if (!await projectService.ProjectExistsAsync(id))
+            {
+                return BadRequest();
+            }
 
             var listOfFiles = Request.Form.Files.ToList();
 
             await photoService.UploadPhotosAsync(id, listOfFiles);
 
-            return View();
+            ViewBag.Message = "Image(s) stored in database!";
+
+            return RedirectToAction(nameof(ProjectController.Details), nameof(ProjectController), new { id });
         }
     }
 }
--- TheHandymanOfCapeCod.Core/Services/PhotoService.cs
+++ TheHandymanOfCapeCod.Core/Services/PhotoService.cs
@@ -1,21 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
 using TheHandymanOfCapeCod.Core.Contracts;
 using TheHandymanOfCapeCod.Infrastructure.Data.Common;
+using TheHandymanOfCapeCod.Infrastructure.Data.Models;
 
 namespace TheHandymanOfCapeCod.Core.Services
 {
     public class PhotoService : IPhotoService
     {
         private readonly IRepository repository;
+        private readonly IProjectService projectService;
 
-        public PhotoService(IRepository _repository)
+        public PhotoService(
+            IRepository _repository,
+            IProjectService _projectService)
         {
             repository = _repository;
+            projectService = _projectService;
         }
 
         public async Task UploadPhotosAsync(int projectId, List<IFormFile> files)
         {
-            
+            var project = await repository.GetByIdAsync<Project>(projectId);
+
+            if (project != null)
+            {
+                foreach (var file in files)
+                {
+                    Photo photo = new Photo();
+
+                    MemoryStream ms = new MemoryStream();
+                    file.CopyTo(ms);
+                    photo.ImageData = ms.ToArray();
+                    photo.ProjectId = projectId;
+
+                    ms.Close();
+                    ms.Dispose();
+
+                    project.Photos.Add(photo);
+
+                    //await repository.AddAsync<Photo>(photo);
+                    
+
+                }
+            }
+
+            await repository.SaveChangesAsync();
         }
     }
 }
